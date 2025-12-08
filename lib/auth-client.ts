4921c377c967d149456c518ca5c28a4610bd25ad
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-12-08 21:54:40 +0300

    feat: added plugins and baseurl

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,3 +1,10 @@
 import { createAuthClient } from "better-auth/react";
+import { inferAdditionalFields } from "better-auth/client/plugins";
+import { auth } from "./auth";
 
-export const { signIn, signUp, signOut, useSession } = createAuthClient();
+export const authClient = createAuthClient({
+  plugins: [inferAdditionalFields<typeof auth>()],
+  baseURL: "http://localhost:3000",
+});
+
+export const { signIn, signUp, signOut, useSession } = authClient;
