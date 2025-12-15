03437157d6bc5c0b21daee5738731bbe21525a62
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-12-15 20:56:01 +0300

    feat(UI): central ui for the app

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "K-Coffee",
+  description: "K-coffee shop website",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
