8c14001379bce54898a1fc7e4350eaabf6b49040
ibn-misbah <kawnuma@gmail.com>
2026-08-22 14:24:19 +0300

    chore: establish clean quality checks

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./prisma";
